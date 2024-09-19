7fa6b01c6db1eece284c24833dbcb32da0bfb52d
Abdarahmane <abda.iba.demba1@gmail.com>
2024-09-19 15:46:56 +0000

    delete home compenent

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import ListCustomers from '../components/customers/customer.vue'; // Ajoute l'extension .vue
 
-import Home from '../components/Home.vue'; 
+
 
 // Importer les composants pour les produits
 import ListProduct from '../components/products/product.vue'; // Liste des produits
@@ -10,7 +10,7 @@ import order from '../components/orders/order.vue'; // Liste des produits
 
 
 const routes = [
-  { path: '/', component: Home },
+  
   { path: '/customers', component: ListCustomers }, // Liste des clients
   
   { path: '/products', component: ListProduct }, // Liste des produits
